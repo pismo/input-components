3f4a8042492b7383198f32177afd9531068dc990
Daniel Paulino <danielpaulino030@gmail.com>
2016-08-26 02:35:16 -0300

    Docs

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,19 @@
+/**
+ * Utility functions
+ * @namespace utils
+ */
+
+/**
+ * getPath - returns the value for the given path
+ * @memberof utils
+ *
+ * @example
+ *  getPath({ foo: { id: 90 } }, 'foo.id') // 90
+ *
+ * @param  {Object} object the object to get the given path
+ * @param  {String} path   the path to get inside the object
+ * @return {Object|undefined} the value for the given path or undefined if the path does not exist
+ */
 export function getPath(object, path) {
     const paths = path.split('.');
     const currentKey = paths[0];
