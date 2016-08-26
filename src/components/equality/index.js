26adce3af9f27bae0ed0199637f6d27ac0ca2f9a
Daniel Paulino <danielpaulino030@gmail.com>
2016-08-26 02:46:28 -0300

    Mais documentacao

--- src/components/equality/index.js
+++ src/components/equality/index.js
@@ -1,10 +1,30 @@
 import angular from 'angular';
 import { getPath } from '../../utils';
 
+/**
+ * angular module to help to validate when one input has the same value than other
+ * @namespace inputEquality
+ */
 const equalityModule = angular
     .module('inputEquality', [])
     .directive('inputEquality', equality);
 
+/**
+ * input-equality - a directive that checks when a input has the same value than other
+ * @name input-equality
+ * @memberof inputEquality
+ * @example
+ * ```html
+ *   <form name="user">
+ *     <input name="password" ng-model="vm.password" />
+ *     <input name="confirm_password" ng-model="vm.confirm_password" input-equality="vm.password" />
+ *     <div ng-messages="user.confirm_password.$error" ng-if="user.confirm_password.$touched">
+ *       <div ng-message="equality">The field must be equal to password</div>
+ *     </div>
+ *   </form>
+ * ```
+ * @return {Directive}  directive definition
+ */
 function equality() {
     return {
         restrict: 'A',
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import angular from 'angular';
 import equalityModule from './components/equality';
 
+/**
+ *  angular module with some directives to help with input validations
+ *  @namespace inputValidations
+ */
 angular
     .module('inputValidations', [equalityModule.name]);
