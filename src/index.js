ca1ec81bc32df2bdc071ea434edc36cfd937919d
Daniel Paulino <danielpaulino030@gmail.com>
2016-08-26 02:48:58 -0300

    default export

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import equalityModule from './components/equality';
  *  angular module with some directives to help with input validations
  *  @namespace inputValidations
  */
-angular
-    .module('inputValidations', [equalityModule.name]);
+const inputValidations = angular.module('inputValidations', [equalityModule.name]);
+
+export default inputValidations;
